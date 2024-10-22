9bf1ffd49658b7b191d72266b2a9da15444b9e9a
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2024-10-22 10:08:16 +0900

    inmemory e2e test

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,56 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { User } from 'src/infra/user/user.entity';
+import { User } from '../src/infra/user/user.entity';
 import { Repository } from 'typeorm';
 import { faker } from '@faker-js/faker';
 import { UserUsecase } from '../src/usecase/userUsecase';
 import { UsersModule } from '../src/users.module';
+import { AppModule } from '../src/app.module';
+import { Login } from '../src/presentation/request/login';
 
-describe('AppController (e2e)', () => {
+describe('UserController (e2e)', () => {
   let app: INestApplication;
   let moduleFixture: TestingModule;
-  // let repo: Repository<User>;
+  let userRepository: Repository<User>;
 
   beforeEach(async () => {
     moduleFixture = await Test.createTestingModule({
       imports: [
-        ConfigModule.forRoot({
-          envFilePath: '.env.test',
-          isGlobal: true,
-        }),
         UsersModule,
         TypeOrmModule.forRoot({
-          type: 'mysql',
-          host: process.env.DATABASE_HOST,
-          port: Number(process.env.DATABASE_PORT),
-          username: process.env.DATABASE_USER,
-          password: process.env.DATABASE_PASSWORD,
-          database: process.env.DATABASE_NAME,
-          entities: ['dist/**/*.entity.{ts,js}'],
-          migrations: ['dist/migrations/*.{ts,js}'],
-          migrationsRun: true,
+          type: 'sqlite',
+          database: ':memory:',
+          entities: [User],
           synchronize: true,
         }),
       ],
-      // providers: [UserUsecase],
     }).compile();
 
-    console.log(process.env);
-
     app = moduleFixture.createNestApplication();
+    userRepository = moduleFixture.get<Repository<User>>(
+      getRepositoryToken(User),
+    );
     await app.init();
   });
 
   it('/user/login (POST)', async () => {
-    const name = faker.name.firstName();
+    const req = new Login();
+    req.nickName = faker.person.firstName();
     const res = await request(app.getHttpServer())
       .post('/user/login')
-      .send({ nickName: name })
-      .expect(200);
-    // repo.findOne({ where: { id: res.body.id } }).then((user) => {
+      .send(req)
+      .expect(HttpStatus.CREATED);
+    console.log(res.body.id);
+    // userRepository.findOne({ where: { id: res.body.id } }).then((user) => {
     //   expect(user).toBeDefined();
     //   expect(user.nickName).toBe(name);
     // });
