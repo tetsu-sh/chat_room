d3c624dd0279a70139af13ed55d4b1e8a37225e7
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2024-10-23 00:21:04 +0900

    create response model

--- src/presentation/userController.ts
+++ src/presentation/userController.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
 import { UserUsecase } from '../usecase/userUsecase';
-import { Login } from './request/login';
+import { LoginRequest } from './request/loginRequest';
+import { LoginResponse } from './response/loginResponse';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userUsecase: UserUsecase) {}
 
   @Post('login')
-  async login(@Body() body: Login): Promise<string> {
-    return await this.userUsecase.login(body.nickName);
+  async login(@Body() body: LoginRequest): Promise<LoginResponse> {
+    var id = await this.userUsecase.login(body.nickName);
+    return new LoginResponse((id = id));
   }
 }
