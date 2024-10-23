6b40ed4ca46f7aa903767bcaafaac13883b640a3
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2024-10-23 10:26:59 +0900

    base entity

--- src/infra/user/user.entity.ts
+++ src/infra/user/user.entity.ts
@@ -1,7 +1,8 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from '../shared/base.entity';
 
 @Entity()
-export class User {
+export class User extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
