1f46130d10ca7dcb775fceb159c5aa831ef65e1e
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2024-10-26 17:38:36 +0900

    nickname is exists then using it

--- src/usecase/userUsecase.ts
+++ src/usecase/userUsecase.ts
@@ -12,6 +12,12 @@ export class UserUsecase {
   ) {}
 
   async login(nickName: string): Promise<string> {
+    var existUser = await this.userRepository.findOne({
+      where: { nickName: nickName },
+    });
+    if (existUser) {
+      return existUser.id;
+    }
     const user = new User();
     user.id = uuid();
     user.nickName = nickName;
