74afa3cfc4d3a0e84c8f437fc3a4af32a7544610
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2024-10-26 13:34:39 +0900

    create seed data

---
+++ seed.sql
@@ -0,0 +1,12 @@
+
+
+
+INSERT INTO users (id,nick_name) VALUES 
+('0192b98d-6d94-74b3-a2e1-3b7b96800950','taro'),
+('0192be87-06cb-7cfd-a3c7-c99f2a9b41e5','jiro')
+;
+
+INSERT INTO chat_rooms(id,name,owner_id) VALUES 
+('0192b991-5077-759e-901f-878dc397bed6','room1','0192b98d-6d94-74b3-a2e1-3b7b96800950')
+;
+
