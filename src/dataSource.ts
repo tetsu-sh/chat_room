d425a3b48fa470a11ceb05167a1cfcf2c21b963c
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2024-10-27 20:47:39 +0900

    rev .env file

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,11 +1,13 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
 import { TypeOrmNamingStrategy } from './config/TypeOrmNamingStrategy';
-dotenv.config({ path: '.env.dev' });
+
+dotenv.config({ path: '.env.development.host' });
+
 const config = {
   type: 'mysql',
-  host: 'localhost',
-  port: Number(3406),
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
