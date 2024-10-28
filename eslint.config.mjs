e81e8d829b7dd504e28c3895de7aad3751b1f020
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2024-10-28 19:31:58 +0900

    tsconfig root dir __dirname

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -39,7 +39,7 @@ export default [
 
       parserOptions: {
         project: 'tsconfig.json',
-        tsconfigRootDir: '/Users/shinmyotetsuforc/workspace/own/chat_room',
+        tsconfigRootDir: __dirname,
       },
     },
 
